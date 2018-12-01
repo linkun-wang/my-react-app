9b393f88c2a96145297e997314dfb0ffbf83c1a8
550575644 <550575644@qq.com>
2018-12-01 20:12:38 +0800

    Updates

---
+++ precache-manifest.204d8f824a6fe4706ea0cfe4a795f78e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "171d95dd39969154bef5",
+    "url": "/my-react-app/static/js/runtime~main.171d95dd.js"
+  },
+  {
+    "revision": "cf5f5c5e96f76603eb1c",
+    "url": "/my-react-app/static/js/main.cf5f5c5e.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/my-react-app/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "cf5f5c5e96f76603eb1c",
+    "url": "/my-react-app/static/css/main.be9e8f23.chunk.css"
+  },
+  {
+    "revision": "6031bf21e35bc134dbb9a01014608cda",
+    "url": "/my-react-app/index.html"
+  }
+];

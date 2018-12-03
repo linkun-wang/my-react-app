86ad2f44116e4a19eba1f5ff5d07d595582e2bca
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-03 19:01:31 +0800

    Update LeftMenu.js

--- src/page/LeftMenu.js
+++ src/page/LeftMenu.js
@@ -20,7 +20,7 @@ export default class LeftMenu extends Component {
     render() {
         return (
             <div>
-                <Menu theme={this.state.theme} defaultSelectedKeys={['/']} defaultOpenKeys={['sub1']} mode="inline">
+                <Menu theme={this.state.theme} defaultSelectedKeys={['/']} selectedKeys={[this.props.match.url]} defaultOpenKeys={['sub1']} mode="inline">
                     <Menu.Item key="/">
                         <Link to="/">
                             <Icon type="home"/>
@@ -29,12 +29,13 @@ export default class LeftMenu extends Component {
                     </Menu.Item>
                     <SubMenu key="sub1" title={<span><Icon type="mail" /><span>信息展示</span></span>}>
                         <MenuItemGroup key="g1" title="个人信息">
-                            <Menu.Item key="1"><Link to="/stars-info">GiHub收藏项目</Link></Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
+                            <Menu.Item key="/stars-info"><Link to="/stars-info">GiHub收藏项目</Link></Menu.Item>
                         </MenuItemGroup>
-                        <MenuItemGroup key="g2" title="其他用户信息">
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
+                        <MenuItemGroup key="g2" title="足迹">
+                            <Menu.Item key="/anYang"><Link to="/anYang">安阳</Link></Menu.Item>
+                            <Menu.Item key="/luoYang"><Link to="/luoYang">洛阳</Link></Menu.Item>
+                            <Menu.Item key="/shangHai"><Link to="/shangHai">上海</Link></Menu.Item>
+                            <Menu.Item key="/hangZhou"><Link to="/hangZhou">杭州</Link></Menu.Item>
                         </MenuItemGroup>
                     </SubMenu>
                     <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>

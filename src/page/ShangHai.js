e0a638e0f88935f345f2c98cf2c1f78668944f62
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-03 19:04:02 +0800

    Create ShangHai.js

---
+++ src/page/ShangHai.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, Table, message, notification } from 'antd';
+import Util  from '../commom/util';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+class ShangHai extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    render() {
+        return (
+            <div style={{ background: '#ECECEC', padding: '30px' }}>
+                <Card hoverable title='"年前回上海来，对于久违了的上海人的第一个印象是白与胖"' bordered={false} style={{ width: 'auto' }}>
+
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default ShangHai;

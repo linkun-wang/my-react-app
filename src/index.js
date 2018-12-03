79f1c0b1e51d86c5e59e96053dfbb026e8f1b07f
550575644 <550575644@qq.com>
2018-12-03 22:09:44 +0800

    change icon before title, add change the router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render((
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

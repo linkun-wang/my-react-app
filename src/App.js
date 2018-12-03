5f2af09f3c76e333f775d664196338d25099a483
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-03 17:06:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
+import { Layout, Icon } from 'antd';
 import logo from './logo.svg';
+import GitHubLog from './images/GitHub.jpg';
 import './App.css';
+import { LeftMenuRoute, ContentRoute } from './route';
+import Current from './page/CurrentDate';
+
+const { Footer, Content, Sider } = Layout;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false
+        }
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
+        return (
+            <div style={{height: '100%'}}>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <span className="left-time"><Current></Current></span>
+                    <div className="center-title">这是一个很丑很丑的网页</div>
+                </header>
+                <Layout style={{height: '95%'}}>
+                    <Sider className="fixed" collapsible trigger={null} collapsed={this.state.collapsed}>
+                       <LeftMenuRoute/>
+                        <Icon className={this.state.collapsed ? 'trigger width-80' : 'trigger width-200'}
+                              type={this.state.collapsed ? 'double-right' : 'double-left'}
+                              onClick={this.toggle}/>
+                    </Sider>
+                    <Layout className={this.state.collapsed ? 'content-normal' : 'content-max'} >
+                        <Content style={{ margin: 0, minHeight: 280 }}>
+                           <ContentRoute />
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>
+                            Fork me on the <a target="_Blank" href="https://github.com/linkun-wang/my-app"><img src={ GitHubLog } className="GitHub-logo"/>GitHub</a>, created by linKun @2018, currently under learning...
+                        </Footer>
+                    </Layout>
+                </Layout>
+                {/*<footer className="footer"></footer>*/}
+            </div>
+        );
+    }
 }
 
 export default App;

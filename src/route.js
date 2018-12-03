ed2824cd316f3cb1bcde70f47d018defa706f327
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-03 19:02:18 +0800

    Update route.js

--- src/route.js
+++ src/route.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import LeftMenu from './page/LeftMenu';
 import Overview from './page/Overview';
-import Option1 from './page/GitHubInfo';
+import GitHubStars from './page/GitHubInfo';
+import AnYang from './page/AnYang';
+import LuoYang from './page/LuoYang';
+import ShangHai from './page/ShangHai';
+import HangZhou from './page/HangZhou';
 
 export const LeftMenuRoute = () =>
     <Route path="*" component={ LeftMenu }/>
@@ -10,7 +14,11 @@ export const LeftMenuRoute = () =>
 export const ContentRoute = () =>
     <Switch>
         <Route exact path="/" component={ Overview }/>
-        <Route exact path="/stars-info" component={ Option1 }/>
+        <Route exact path="/stars-info" component={ GitHubStars }/>
+        <Route exact path="/anYang" component={ AnYang }/>
+        <Route exact path="/luoYang" component={ LuoYang }/>
+        <Route exact path="/shangHai" component={ ShangHai }/>
+        <Route exact path="/hangZhou" component={ HangZhou }/>
     </Switch>
 
 export default ContentRoute

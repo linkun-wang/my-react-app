707475b6b7c9f1639d2332a4c423a54f48a87bce
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-03 19:04:18 +0800

    Create HangZhou.js

---
+++ src/page/HangZhou.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, Table, message, notification } from 'antd';
+import Util  from '../commom/util';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+class HangZhou extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    render() {
+        return (
+            <div style={{ background: '#ECECEC', padding: '30px' }}>
+                <Card hoverable title='"江南忆，最忆是杭城"' bordered={false} style={{ width: 'auto' }}>
+
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default HangZhou;

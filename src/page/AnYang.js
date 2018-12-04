86dc8491e22fd493989c041bddb72929d7fee520
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-04 19:33:00 +0800

    Update AnYang.js

--- src/page/AnYang.js
+++ src/page/AnYang.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Table, message, notification } from 'antd';
 import Util from '../common/util';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Map, Marker } from 'react-amap';
 
 class AnYang extends React.Component {
     constructor(props) {
@@ -9,6 +9,29 @@ class AnYang extends React.Component {
         this.state = {
             loading: false
         };
+        this.mapParams = {
+            amapKey: Util.constant.MAP_KEY,
+            position: { longitude: 114.352482, latitude: 36.103442 },
+            zoom: 6,
+            plugins: ['Scale', //比例尺
+                {
+                    name: 'OverView', //鹰眼
+                    options: {
+                        isOpen: true
+                    }
+                }, {
+                    name: 'ToolBar',
+                    options: {
+                        liteStyle: true, // 精简模式
+                        ruler: true, // 标尺键盘是否可见
+                        noIpLocate: true, // 定位失败后，是否开启IP定位
+                        locate: true, // 是否显示定位按钮
+                        autoPosition: false, // 是否在地图初始化完成后自动定位
+                        position: 'RT' // 位置，可选：LT RT LB RB
+                    },
+                }]
+        };
+        this.markerPosition = this.mapParams.position;
     }
 
     componentDidMount() {
@@ -23,7 +46,16 @@ class AnYang extends React.Component {
         return (
             <div style={{ background: '#ECECEC', padding: '30px' }}>
                 <Card hoverable title='"天有玄鸟，降而生商"' bordered={false} style={{ width: 'auto' }}>
-
+                    <div style={{width:'auto', height:700}}>
+                        <Map amapkey={ this.mapParams.amapKey }
+                             center={ this.mapParams.position }
+                             zoom={ this.mapParams.zoom }
+                             plugins={ this.mapParams.plugins }>
+                            <Marker position={this.markerPosition}
+                                    animation="AMAP_ANIMATION_BOUNCE">
+                            </Marker>
+                        </Map>
+                    </div>
                 </Card>
             </div>
         )

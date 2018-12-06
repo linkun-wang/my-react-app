10dc1966273c78fea0ff67fa9e4cedd89ec0a118
linkun <33945539+linkun-wang@users.noreply.github.com>
2018-12-06 20:44:43 +0800

    Update LeftMenu.js

--- src/page/LeftMenu.js
+++ src/page/LeftMenu.js
@@ -32,8 +32,8 @@ export default class LeftMenu extends Component {
                             <Menu.Item key="/stars-info"><Link to="/stars-info">GiHub收藏项目</Link></Menu.Item>
                             <Menu.Item key="/footprint"><Link to="/footprint" replace>足迹</Link></Menu.Item>
                         </MenuItemGroup>
-                        <MenuItemGroup key="g2" title="足迹">
-                            <Menu.Item key="/anYang"><Link to="/anYang">安阳</Link></Menu.Item>
+                        <MenuItemGroup key="g2" title="Notes">
+                            <Menu.Item key="/anYang"><Link to="/learnSVG">SVG学习</Link></Menu.Item>
                             <Menu.Item key="/luoYang"><Link to="/luoYang">洛阳</Link></Menu.Item>
                             <Menu.Item key="/shangHai"><Link to="/shangHai">上海</Link></Menu.Item>
                             <Menu.Item key="/hangZhou"><Link to="/hangZhou">杭州</Link></Menu.Item>

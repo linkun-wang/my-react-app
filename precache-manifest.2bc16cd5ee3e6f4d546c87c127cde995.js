db193745421d3c7485cf13eff4b8a902e3c74d8e
linkun-wang <linkun0922@163.com>
2021-01-13 16:58:35 +0800

    Updates

---
+++ precache-manifest.2bc16cd5ee3e6f4d546c87c127cde995.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "10f14441f68721d3d2fe",
+    "url": "/my-react-app/static/css/main.a8187405.chunk.css"
+  },
+  {
+    "revision": "10f14441f68721d3d2fe",
+    "url": "/my-react-app/static/js/main.5352259a.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "297e12d79bd914256599",
+    "url": "/my-react-app/static/css/2.fd605723.chunk.css"
+  },
+  {
+    "revision": "297e12d79bd914256599",
+    "url": "/my-react-app/static/js/2.119918b1.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "115b00d89d0319a537367476c4629b54",
+    "url": "/my-react-app/static/media/GitHub.115b00d8.jpg"
+  },
+  {
+    "revision": "55b54ea579b191841358d41a712d0b10",
+    "url": "/my-react-app/static/media/Leon.55b54ea5.jpeg"
+  },
+  {
+    "revision": "c49920df25fbb7f8e32bdc85c3d8a722",
+    "url": "/my-react-app/static/media/Totoro.c49920df.gif"
+  },
+  {
+    "revision": "012aee4ea4085d21192d2e9b94412440",
+    "url": "/my-react-app/static/media/weightlifting.012aee4e.gif"
+  },
+  {
+    "revision": "cb2dce8c7d9aebe86106a936099141b8",
+    "url": "/my-react-app/static/media/汉仪雪君体繁.cb2dce8c.ttf"
+  },
+  {
+    "revision": "0b225eaad62f9fe65be6f6077f6a8ee7",
+    "url": "/my-react-app/static/media/汉仪雪君体繁.0b225eaa.eot"
+  },
+  {
+    "revision": "2a15844d51f959bb2017a551524a3c94",
+    "url": "/my-react-app/static/media/汉仪雪君体繁.2a15844d.woff"
+  },
+  {
+    "revision": "5af3b582e73b855a2a518ebb989c1817",
+    "url": "/my-react-app/static/media/汉仪雪君体繁.5af3b582.svg"
+  },
+  {
+    "revision": "f8a7c1f414bd23eeb0246600c7a3047f",
+    "url": "/my-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.1954d7b3e60404059d7efce6ff66ffab.js"
+  "/my-react-app/precache-manifest.2bc16cd5ee3e6f4d546c87c127cde995.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/my-react-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
